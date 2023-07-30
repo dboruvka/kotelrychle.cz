ed3a0fbf4f35cc03e92fe79224e22edabef1e6df
dboruvka <david.boruvka@gmail.com>
2023-07-30 03:41:41 +0200

    částečně hotová patička a hlavička - základ, není mobilní verze

---
+++ scripts.footer.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+//    $('#signature').append(' &amp; <a href="https://davidboruvka.cz/?utm_source=footer&amp;utm_medium=link&amp;utm_campaign=shoptet" target="_blank" title="Tvorba eshopů na shoptetu">David Borůvka</a>');
+
+$(".top-nav-button-login").insertBefore($(".cart-count")); // Přesuneme prvek před prvek .cart-count
+
+$(".top-nav-button-login > span").addClass("visible-lg-inline-block");
+
+$("#footer .container").wrapAll('<div class="conteiner-max-width"></div>');
+$("#footer .footer-bottom").wrapAll('<div class="conteiner-max-width2"></div>');
+
+});
