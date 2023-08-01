0f5c52021cf805e841f5978dc464e3b9939d39fd
dboruvka <david.boruvka@gmail.com>
2023-08-01 16:26:10 +0200

    úprava detailu produktu

--- scripts.footer.js
+++ scripts.footer.js
@@ -12,6 +12,29 @@ $("#footer .container").wrapAll('<div class="conteiner-max-width"></div>');
 $("#footer .footer-bottom").wrapAll('<div class="conteiner-max-width2"></div>');
 
 
+if (dataLayer[0].shoptet.pageType === "productDetail") // detail produktu
+{
+    $("h1").insertBefore($(".p-final-price-wrapper")); // přesune  nadpis do pravé části
+    $(".p-detail-info > .stars-wrapper").insertAfter($("h1")); // přesune hodnocení pod H1
+    $(".p-detail-inner-header > .p-code").insertAfter($(".stars-wrapper")); // přesune náš kód
+    $(".stars-wrapper").wrap(".line-start"); // o
+    $(".p-info-wrapper .stars-wrapper, .p-info-wrapper .p-code").wrapAll("<div class='stars-info-wrapper'></div>"); // obalí .stars-wrapper a .p-code do <div class='wrap-stars'>
+
+
+
+    
+    //$("p-info-wrapper > .stars-wrapper").insertAfter($(".stars-wrapper")); // přesune náš kód
+    
+    
+    
+    //$(".p-code").insertAfter($(".stars-wrapper")); // přesune hodnocení pod H1
+    
+    
+
+
+
+}
+
 
 //$('.slider-header  .to').after('<span class="db-az">až</span>');
 //$('.slider-header').insertAfter('.slider-wrapper');
