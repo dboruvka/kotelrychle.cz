db97522bd3ddd924bced8e65b87a385b8f4aefcb
dboruvka <david.boruvka@gmail.com>
2023-08-23 16:59:28 +0200

    úprava hlavní banner

--- scripts.footer.js
+++ scripts.footer.js
@@ -255,6 +255,12 @@ $("span.flag-custom2:contains('Český produkt')").insertAfter(".detail-parametr
 
     
   $(".welcome-wrapper h1").remove();
+  $(".in-index .banners-row .col-sm-4").remove();
+
+  $(".in-index .banners-row .col-sm-8").removeClass("col-sm-8").addClass("col-sm-12");
+  
+  
+
     
   $("span.flag-custom2:contains('Český produkt')").each(function() {
         var flag_remove = $(this).closest(".product").find(".p-in-in");
