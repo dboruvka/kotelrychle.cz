328dbe3e9eb597ccdc3555f024c3397306853e38
dboruvka <david.boruvka@gmail.com>
2023-10-19 18:29:28 +0200

    subkategorie

---
+++ scripts.header.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+
+    alert("d");
+    //$(".p-image-wrapper .flags-extra .flag-freeshipping span.flag-text ").text("Doprava zdarma");
+    //$(".p-image-wrapper .flags-extra .flag-text").css("color","red");
+    
+
+});
